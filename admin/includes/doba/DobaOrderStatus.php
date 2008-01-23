a426493b543a2640376ba2095d2d55abac62aae9
shiquemano <shiquemano@e2e3bf7e-a443-0410-a111-f9566b15aaec>
2008-01-23 00:28:31 +0000

    new include folder for doba libraries

---
+++ admin/includes/doba/DobaOrderStatus.php
@@ -0,0 +1,27 @@
+<?php
+class DobaOrderStatus {
+	/**
+	 * @var string 
+	 */
+	var $status;
+	/**
+	 * @var float
+	 */
+	var $subtotal;
+	/**
+	 * @var float
+	 */
+	var $service_fees;
+	var $shipping_fees;
+	var $dropship_fees;
+	var $total;
+	var $name;
+	var $address;
+	var $city;
+	var $state;
+	var $postal;
+	var $country;
+	var $items;
+	var $shipments;
+}
+?>
