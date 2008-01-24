1df888715a18fbc88a35d93520902e4882b78019
shiquemano <shiquemano@e2e3bf7e-a443-0410-a111-f9566b15aaec>
2008-01-24 23:23:11 +0000

    shanged admin files to include initial doba config pages

--- admin/includes/languages/english/doba_configure.php
+++ admin/includes/languages/english/doba_configure.php
@@ -11,17 +11,4 @@
 */
 
 define('HEADING_TITLE', 'Doba Configuration');
-
-define('TABLE_HEADING_ADMINISTRATORS', 'Administrators');
-define('TABLE_HEADING_ACTION', 'Action');
-
-define('TEXT_INFO_INSERT_INTRO', 'Please enter the new administrator with its related data');
-define('TEXT_INFO_EDIT_INTRO', 'Please make any necessary changes');
-define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete this administrator?');
-define('TEXT_INFO_HEADING_NEW_ADMINISTRATOR', 'New Administrator');
-define('TEXT_INFO_USERNAME', 'Username:');
-define('TEXT_INFO_NEW_PASSWORD', 'New Password:');
-define('TEXT_INFO_PASSWORD', 'Password:');
-
-define('ERROR_ADMINISTRATOR_EXISTS', 'Error: Administrator already exists.');
 ?>
