70e4fea588bcf42fc201dea401126236d05f7f04
SeanTLC <SeanTLC@e2e3bf7e-a443-0410-a111-f9566b15aaec>
2008-01-29 22:50:59 +0000

    Update to DobaProdustFile almost done with first pass on ProccessFile function

--- trunk/admin/includes/doba/DobaProductFile.php
+++ trunk/admin/includes/doba/DobaProductFile.php
@@ -51,21 +51,29 @@ class DobaProductFile {
 			$values = explode($delm, $data);
 	
 			/* Fields to fill in on the osCommerce add product page
-			 * Products Status:   In Stock,  Out of Stock 
-			 * Date Available
-			 * Products Manufacturer
-			 * Products Name (English, Spanish, Ducth)
-			 * Tax Class: none, taxable goods
-			 * Products Price (Net)
-			 * Products Description (English, Spanish, Ducth)
-			 * Products Quantity
-			 * Products Model
-			 * Products Image
-			 * Products URL (English, Spanish, Ducth)
-			 * Products Weight
+			 * Products Status:   In Stock,  Out of Stock		
+			 * Date Available									
+			 * Products Manufacturer							? How do I get this info (Not part or Doba file)
+			 * Products Name (English, Spanish, Ducth)			/
+			 * Tax Class: none, taxable goods					? How do I get this info (Not part or Doba file)
+			 * Products Price (Net)								. Will use the MSRP for now.
+			 * Products Description (English, Spanish, Ducth)	/
+			 * Products Quantity								. Will just use the amount in the file till a better solution is found
+			 * Products Model									? 
+			 * Products Image									. 
+			 * Products URL (English, Spanish, Ducth)			/
+			 * Products Weight									/
 			 */
 			
 			$tempDPD->product_id($values[(int)array_keys($headers, 'PRODUCT_ID')]);
+			$tempDPD->item_id($values[(int)array_keys($headers, 'ITEM_ID')]);
+			$tempDPD->$title($values[(int)array_keys($headers, 'TITLE')]);
+			$tempDPD->$price($values[(int)array_keys($headers, 'MSRP')]);
+			$tempDPD->$description(''.$values[(int)array_keys($headers, 'DESCRIPTION')].$values[(int)array_keys($headers, 'DETAILS')]);
+			$tempDPD->$quantity($values[(int)array_keys($headers, 'QTY_AVAIL')]);
+			$tempDPD->$image_url($values[(int)array_keys($headers, 'IMAGE_URL')]);
+			$tempDPD->$ship_weight($values[(int)array_keys($headers, 'WEIGHT')]);			
+
 			$DobaProds->addProduct($tempDPD);
 		} 
 		
