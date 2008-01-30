803559a5202daab86b3f3dd4e3126d68eb81076c
SeanTLC <SeanTLC@e2e3bf7e-a443-0410-a111-f9566b15aaec>
2008-01-30 00:55:12 +0000

    More updates

--- admin/includes/doba/DobaProductFile.php
+++ admin/includes/doba/DobaProductFile.php
@@ -41,7 +41,9 @@ class DobaProductFile {
 		{
 			$data = fgets($fp);
 			
-			$headers = explode($delm, $data);			
+			$headers = explode($delm, $data);	
+	
+			array_walk($fruits, 'strtoupper');
 		}
 		
 		while(!feof($fp)) 
@@ -51,29 +53,32 @@ class DobaProductFile {
 			$values = explode($delm, $data);
 	
 			/* Fields to fill in on the osCommerce add product page
-			 * Products Status:   In Stock,  Out of Stock		
-			 * Date Available									
-			 * Products Manufacturer							? How do I get this info (Not part or Doba file)
+			 * Products Status:   In Stock,  Out of Stock		. Passing the current available in the object, needs to be processed before being added to database
+			 * Date Available									/
+			 * Products Manufacturer							. Leaving blank
 			 * Products Name (English, Spanish, Ducth)			/
-			 * Tax Class: none, taxable goods					? How do I get this info (Not part or Doba file)
+			 * Tax Class: none, taxable goods					. Default to Taxable, will need setting in the osCommerce config files
 			 * Products Price (Net)								. Will use the MSRP for now.
 			 * Products Description (English, Spanish, Ducth)	/
 			 * Products Quantity								. Will just use the amount in the file till a better solution is found
-			 * Products Model									? 
-			 * Products Image									. 
-			 * Products URL (English, Spanish, Ducth)			/
+			 * Products Model									/ = Product SKU
+			 * Products Image									. Will need more processing while the object in being added to the database
+			 * Products URL (English, Spanish, Ducth)			/ Leaveing blank
 			 * Products Weight									/
 			 */
 			
 			$tempDPD->product_id($values[(int)array_keys($headers, 'PRODUCT_ID')]);
 			$tempDPD->item_id($values[(int)array_keys($headers, 'ITEM_ID')]);
-			$tempDPD->$title($values[(int)array_keys($headers, 'TITLE')]);
-			$tempDPD->$price($values[(int)array_keys($headers, 'MSRP')]);
-			$tempDPD->$description(''.$values[(int)array_keys($headers, 'DESCRIPTION')].$values[(int)array_keys($headers, 'DETAILS')]);
-			$tempDPD->$quantity($values[(int)array_keys($headers, 'QTY_AVAIL')]);
-			$tempDPD->$image_url($values[(int)array_keys($headers, 'IMAGE_URL')]);
-			$tempDPD->$ship_weight($values[(int)array_keys($headers, 'WEIGHT')]);			
-
+			$tempDPD->title($values[(int)array_keys($headers, 'TITLE')]);
+			$tempDPD->price($values[(int)array_keys($headers, 'MSRP')]);
+			$tempDPD->description(''.$values[(int)array_keys($headers, 'DESCRIPTION')].'<br>'.$values[(int)array_keys($headers, 'DETAILS')]);
+			$tempDPD->quantity($values[(int)array_keys($headers, 'QTY_AVAIL')]);
+			$tempDPD->image_url($values[(int)array_keys($headers, 'IMAGE_URL')]);
+			$tempDPD->ship_weight($values[(int)array_keys($headers, 'WEIGHT')]);
+			$tempDPD->date_avail(date('Y-m-d'));
+			$tempDPD->product_sku($values[(int)array_keys($headers, 'SKU')]);
+			
+			
 			$DobaProds->addProduct($tempDPD);
 		} 
 		
