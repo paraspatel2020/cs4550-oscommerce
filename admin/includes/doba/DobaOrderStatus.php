9b0317a6bb6f17bdb5a991159b0fda37ddc6b36d
sherryle.barker <sherryle.barker@e2e3bf7e-a443-0410-a111-f9566b15aaec>
2008-04-15 17:14:14 +0000

    added documentation

--- admin/includes/doba/DobaOrderStatus.php
+++ admin/includes/doba/DobaOrderStatus.php
@@ -12,16 +12,51 @@ class DobaOrderStatus {
 	 * @var float
 	 */
 	var $service_fees;
+	/**
+	 * @var float
+	 */
 	var $shipping_fees;
+	/**
+	 * @var float
+	 */
 	var $dropship_fees;
+	/**
+	 * @var float
+	 */
 	var $total;
+	/**
+	 * @var string 
+	 */
 	var $name;
+	/**
+	 * @var string 
+	 */
 	var $address;
+	/**
+	 * @var string 
+	 */
 	var $city;
+	/**
+	 * @var string 
+	 */
 	var $state;
+	/**
+	 * @var string 
+	 */
 	var $postal;
+	/**
+	 * @var string 
+	 */
 	var $country;
+	/**
+	 * @var array
+	 * id, distributor_id, status
+	 */
 	var $items;
+	/**
+	 * @var array
+	 * distributor_id, carrier, tracking
+	 */
 	var $shipments;
 }
 ?>
